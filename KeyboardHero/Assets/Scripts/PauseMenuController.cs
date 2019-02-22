91a83384211c4f2ef56f827f8666d902a8f53c04
Connor Lucier <connor@connorlucier.com>
2019-02-21 17:03:46 -0800

    Added pause menu while playing song

---
+++ KeyboardHero/Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,50 @@
+﻿using AudioHelm;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour {
+
+    public static bool gamePaused = false;
+
+    public GameObject pauseMenuUI;
+    public AudioHelmClock clock;
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+                Resume();
+            else
+                Pause();
+
+            gamePaused = !gamePaused;
+        }
+	}
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        clock.pause = true;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        clock.pause = false;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
